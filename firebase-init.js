522bd368105025608af22671632ff9aa95ba585d
Genarex6 <labarreracaballero@gmail.com>
2026-07-29 10:32:48 -0500

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,11 @@
 /* Configuración pública de la aplicación web Firebase. */
 const firebaseConfig = {
-  apiKey: "AIzaSyBFCxKZjEdjVHyzT5z1kEv08TOKdPK3Q6aA",
+  apiKey: "AIzaSyBFcXgzEdjVHyzT5z1kEvO8TOKdPK3Q6aA",
   authDomain: "control-financiero-7602e.firebaseapp.com",
   projectId: "control-financiero-7602e",
   storageBucket: "control-financiero-7602e.firebasestorage.app",
   messagingSenderId: "420459768760",
-  appId: "1:420459768760:web:9f9371b3e6af2481f3d4bd"
+  appId: "1:420459768760:web:79f3371b3e6af2481f3d4d"
 };
 
 firebase.initializeApp(firebaseConfig);
